1a4cb87e26a7c4b6f2fae15f9f78241c0709e56b
Nikolai Vorontsov <nickviz@telenet.be>
2024-01-27 17:51:27 +0100

    Added process detection

--- src/CompatKiller.cpp
+++ src/CompatKiller.cpp
@@ -23,7 +23,78 @@
 // THE SOFTWARE.
 ////////////////////////////////////////////////////////////////////////////////
 
-static int Help()
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <psapi.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <vector>
+#include <string>
+#pragma comment(lib, "psapi.lib") 
+
+// This function returns vector DWORD filled in with the current PIDs
+static std::vector<DWORD> getProcIDs()
+{
+	std::vector<DWORD> out(200, 0); // Init size;
+	try
+	{	// Get the list of process identifiers.
+		DWORD cbNeeded = -1;
+		while (::EnumProcesses(out.data(), out.size() * sizeof DWORD, &cbNeeded) &&
+							   cbNeeded >= out.size() * sizeof DWORD)
+			out.resize(cbNeeded / sizeof DWORD + 1);
+	}
+	catch (...) // memory
+	{
+		out.clear();
+	}
+	return out;
+}
+
+static std::string getProcessName(DWORD PID)
+{
+	TCHAR szName[2 * MAX_PATH];
+	szName[0] = 0;
+
+	// Get a handle to the process.
+	HANDLE hProcess = ::OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, PID);
+	if (hProcess == NULL)
+		return szName;
+
+	// Get the process name.
+	DWORD dwLen = ::GetModuleBaseName(hProcess, 0, szName, std::size(szName));
+	if (dwLen == 0 && GetLastError() == ERROR_PARTIAL_COPY)
+	{	// Trying to open 64-bit process from 32-bit one
+		dwLen = GetProcessImageFileName(hProcess, szName, std::size(szName));
+		::CloseHandle(hProcess);
+		if (dwLen > 0)
+		{
+			LPCTSTR pSlash = _tcsrchr(szName, _T('\\'));
+			return pSlash ? pSlash + 1 : szName;
+		}
+	}
+	else
+		::CloseHandle(hProcess);
+	return szName;
+}
+
+static bool isProcessRunning(LPCTSTR szName)
+{
+	auto processes = getProcIDs();
+	if (processes.empty())
+		return false;
+
+	// Scan through the all and check for names
+	for (auto ID : processes)
+	{
+		auto name = getProcessName(ID);
+		if (_tcsicmp(name.c_str(), szName) == 0)
+			return true;
+	}
+	return false;
+}
+
+
+static int help()
 {
 	_tprintf(_T("This application is intended to kill annoying CompatTelRunner process\n\n"));
 	return 0;
@@ -36,6 +107,13 @@ int _tmain(int argc, _TCHAR* argv[])
 		_T( " x64")
 #endif
 		_T(". (C) 2024 VorontSOFT. Version 1.0\n"));
+
+	while (true)
+	{
+		if (isProcessRunning(_T("CompatTelRunner.exe")))
+			printf("Found\n");
+		_sleep(2000);
+	}
 	
-	return Help();
+	return help();
 }
--- src/pch.h
+++ src/pch.h
@@ -2,7 +2,11 @@
 // or project specific include files that are used frequently, but
 // are changed infrequently
 #pragma once
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <psapi.h>
 #include <stdio.h>
 #include <tchar.h>
+#include <vector>
+#include <string>
 
